f4e7cc69b328f0ba163b050f11dcff6e8cac1eb1
Joao Curti <jpcurti@users.noreply.github.com>
2024-05-26 23:27:54 +0200

    Map encoder states to same structure as expected by the display interface

--- klippy/extras/e3v3se_display.py
+++ klippy/extras/e3v3se_display.py
@@ -2,8 +2,15 @@ import logging
 import time
 from .display import menu_keys
 
-
 class E3v3seDisplay:
+    
+    ENCODER_DIFF_NO = 0  # no state
+    ENCODER_DIFF_CW = 1  # clockwise rotation
+    ENCODER_DIFF_CCW = 2  # counterclockwise rotation
+    ENCODER_DIFF_ENTER = 3  # click
+
+    EncoderRateLimit = True
+
     def __init__(self, config):
         self.printer = config.get_printer()
         self.config = config
@@ -13,6 +20,7 @@ class E3v3seDisplay:
         self._logging = config.getboolean("logging", False)
         self.gcode = self.printer.lookup_object("gcode")
         self.printer.register_event_handler("klippy:ready", self.handle_ready)
+        self.encoder_state = self.ENCODER_DIFF_NO
 
         # register for key events
         menu_keys.MenuKeys(config, self.key_event)
@@ -22,13 +30,28 @@ class E3v3seDisplay:
 
     def key_event(self, key, eventtime):
         if key == 'click':
-            self.log("click pressed.")
+            self.log("click")
+            self.encoder_state = self.ENCODER_DIFF_ENTER
         elif key == 'long_click':
-            self.log("long_click pressed.")
+            self.log("long_click")
+            self.encoder_state = self.ENCODER_DIFF_ENTER
         elif key == 'up':
-             self.log("up pressed ")
+            self.log("up")
+            self.encoder_state = self.ENCODER_DIFF_CCW
         elif key == 'down':
-             self.log("down pressed ")
+            self.log("down")
+            self.encoder_state = self.ENCODER_DIFF_CW
+
+        self.encoder_has_data()
+
+    def get_encoder_state(self):
+        last_state = self.encoder_state
+        self.encoder_state = self.ENCODER_DIFF_NO
+        return  last_state
+
+    def encoder_has_data(self):
+        self.log("Key event")
+        return 
 
     def _screen_update(self, eventtime):
         #self.log("Display update: ")
@@ -55,9 +78,6 @@ class E3v3seDisplay:
 
     def error(self, msg, *args, **kwargs):
         logging.error("E3V3SE Display: " + str(msg))
-
-    def get_encoder_state(self):
-        return self.encoder.getValue()
 
 def load_config(config):
     return E3v3seDisplay(config)
